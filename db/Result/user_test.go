ed9c75bf65512f0c6ab8d25c7c54bea790dacd67
Nilesh Khodiyar <nileshkhodiyar2000@gmail.com>
2025-08-23 10:59:02 +0000

    user test file

---
+++ db/Result/user_test.go
@@ -0,0 +1,49 @@
+package Anuskh
+
+import (
+	"context"
+	"testing"
+
+	"github.com/nilesh0729/OrdinaryBank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func CreateRandomUser(t *testing.T) User{
+	arg := CreateUserParams{
+		Username: util.RandomOwner(),
+		HashedPassword: "secret",
+		FullName: util.RandomOwner(),
+		Email: util.RandomEmail(),
+	}
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, user.Username, arg.Username)
+	require.Equal(t, user.HashedPassword, arg.HashedPassword)
+	require.Equal(t, user.FullName, arg.FullName)
+	require.Equal(t, user.Email, arg.Email)
+
+	require.True(t,user.PasswordChangedAt.IsZero())
+	require.NotZero(t , user.CreatedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T){
+	CreateRandomUser(t)
+}
+
+func TestGetUser(t *testing.T){
+	user := CreateRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(),user.Username)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user2.Username, user.Username)
+	require.Equal(t, user2.HashedPassword, user.HashedPassword)
+	require.Equal(t, user2.FullName, user.FullName)
+	require.Equal(t, user2.Email, user.Email)
+
+	require.NotZero(t, user2.CreatedAt, user.CreatedAt)
+}
