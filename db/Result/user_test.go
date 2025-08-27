a478e325ec3699ecdde72475f3242121d3294f2e
Nilesh Khodiyar <nileshkhodiyar2000@gmail.com>
2025-08-27 09:37:23 +0000

    implemented Password hashing.

--- db/Result/user_test.go
+++ db/Result/user_test.go
@@ -9,9 +9,12 @@ import (
 )
 
 func CreateRandomUser(t *testing.T) User{
+	hashPassword, err:= util.HashedPassword(util.RandomString(8))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username: util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashPassword,
 		FullName: util.RandomOwner(),
 		Email: util.RandomEmail(),
 	}
