9511e7aa67a75968ef0ea3b8cf7125940d4e14cc
Nilesh Khodiyar <nileshkhodiyar2000@gmail.com>
2025-09-08 09:52:17 +0000

    Created jwt Token Test file

---
+++ token/jwt_maker_test.go
@@ -0,0 +1,68 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/nilesh0729/OrdinaryBank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJWTMaker(t *testing.T) {
+	maker, err := NewJWTMAKER(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	Duration := time.Minute
+
+	IssuedAt := time.Now()
+	ExpiredAt := IssuedAt.Add(Duration)
+
+	token, err := maker.CreateToken(username, Duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, payload.Username, username)
+	require.WithinDuration(t, IssuedAt, payload.IssuedAt.Local(), time.Second)
+	require.WithinDuration(t, ExpiredAt, payload.ExpiresAt.Local(), time.Second)
+}
+
+func TestExpiredJWTToken(t *testing.T) {
+	maker, err := NewJWTMAKER(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+
+}
+
+func TestInvalidTokenAlgNone(t *testing.T){
+	payload, err := NewPayload(util.RandomOwner(),time.Minute)
+	require.NoError(t, err)
+
+	maker, err := NewJWTMAKER(util.RandomString(32))
+	require.NoError(t, err)
+	
+	jwtToken :=  jwt.NewWithClaims(jwt.SigningMethodNone,payload)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+	require.Nil(t, payload)
+
+	
+}
